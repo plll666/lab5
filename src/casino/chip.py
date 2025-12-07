1742505d723960919cc14fe22fd5c8f6d91e841d
plll666 <kolalazzzz@gmail.com>
2025-12-07 22:44:01 +0300

    добавил валидацию данных и исправил методы

--- src/casino/chip.py
+++ src/casino/chip.py
@@ -1,6 +1,9 @@
-
 class Chip:
     def __init__(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("Значение фишки должно быть целым числом")
+        if value < 0:
+            raise ValueError("Значение фишки не может быть отрицательным")
         self.value = value
 
     def __add__(self, other):
@@ -8,6 +11,12 @@ class Chip:
             return Chip(self.value + other.value)
         elif isinstance(other, int):
             return Chip(self.value + other)
+        return NotImplemented
+
+    def __radd__(self, other):
+        if isinstance(other, int):
+            return Chip(self.value + other)
+        return NotImplemented
 
     def __repr__(self):
         return f"Фишка: {self.value} "
