696896c68662d8c60d3b0c8392d7b36fcfae37d5
plll666 <kolalazzzz@gmail.com>
2025-12-07 23:00:19 +0300

    добавил недостающие методы

--- src/gooses/goose_collection.py
+++ src/gooses/goose_collection.py
@@ -1,18 +1,11 @@
-from src.gooses.Goose import Goose
+from src.gooses.Goose import Goose, WarGoose, HonkGoose
 
 class GooseCollection:
     def __init__(self):
         self._gooses = []
 
-    def __len__(self):
-        return len(self._gooses)
-    def __getitem__(self, item):
-        return self._gooses[item]
-    def __iter__(self):
-        return iter(self._gooses)
-
     def add(self, goose: Goose):
-        if not isinstance(goose, Goose):
+        if not isinstance(goose, (Goose, WarGoose, HonkGoose)):
             raise TypeError(f"{goose} должен быть гусем")
 
         if goose in self._gooses:
@@ -22,7 +15,7 @@ class GooseCollection:
         print(f"Гусь '{goose.name}' добавлен")
 
     def remove(self, goose):
-        if isinstance(goose, Goose):
+        if isinstance(goose, (Goose, WarGoose, HonkGoose)):
             if goose not in self._gooses:
                 raise ValueError("Гусь не найден")
             self._gooses.remove(goose)
@@ -40,5 +33,34 @@ class GooseCollection:
         else:
             raise TypeError("remove принимает Goose или имя")
 
+    def __len__(self):
+        return len(self._gooses)
 
+    def __iter__(self):
+        return iter(self._gooses)
 
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            return GooseCollection(self._gooses[index])
+        return self._gooses[index]
+
+    def __contains__(self, item):
+        if isinstance(item, (Goose, WarGoose, HonkGoose)):
+            return item in self._gooses
+        if isinstance(item, str):
+            return any(g.name == item for g in self._gooses)
+        return False
+
+    def find_by_name(self, name):
+        for g in self._gooses:
+            if g.name == name:
+                return g
+        return None
+
+    def find_by_type(self, goose_type):
+        """Поиск гусей по типу"""
+        result = GooseCollection()
+        for g in self._gooses:
+            if isinstance(g, goose_type):
+                result.add(g)
+        return result
