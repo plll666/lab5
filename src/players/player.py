ba657af26d287c5670c038dc38a114e8ed730779
plll666 <kolalazzzz@gmail.com>
2025-12-07 23:00:39 +0300

    добавил валидацию данных

--- src/players/player.py
+++ src/players/player.py
@@ -1,6 +1,12 @@
-
 class Player:
-    def __init__(self,name: str, balance: int):
+    def __init__(self, name: str, balance: int):
+        if not isinstance(name, str):
+            raise TypeError("Имя должно быть строкой")
+        if not isinstance(balance, int):
+            raise TypeError("Баланс должен быть целым числом")
+        if balance < 0:
+            raise ValueError("Баланс не может быть отрицательным")
+
         self.name = name
         self.balance = balance
 
